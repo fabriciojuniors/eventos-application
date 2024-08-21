d868742bb850dcebb6238387fa1970f76c0b3fed
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-08-21 14:56:24 -0300

    Remove testes de exemplo

--- src/test/java/cloud/fabriciojunior/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package cloud.fabriciojunior;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
