1469bce658a56a422416aafd4671a040ad4df6a7
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-08-22 20:16:36 -0300

    Adiciona handler de exceções

---
+++ src/main/java/cloud/fabriciojunior/config/ExceptionMapper.java
@@ -0,0 +1,41 @@
+package cloud.fabriciojunior.config;
+
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import org.hibernate.exception.ConstraintViolationException;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import org.postgresql.util.PSQLException;
+
+import java.util.Map;
+
+import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+
+public class ExceptionMapper {
+
+    @ServerExceptionMapper
+    public Response mapException(final NotFoundException exception) {
+        return Response.status(NOT_FOUND)
+                .entity(Map.of("message", exception.getMessage()))
+                .build();
+    }
+
+    @ServerExceptionMapper
+    public Response mapException(final ConstraintViolationException exception) {
+        final PSQLException psqlException = (PSQLException) exception.getCause();
+        return Response.status(BAD_REQUEST)
+                .entity(Map.of("message", "Restrições cadastrais violadas",
+                        "details", psqlException.getServerErrorMessage().getDetail(),
+                        "errorCode", psqlException.getSQLState()))
+                .build();
+    }
+
+    @ServerExceptionMapper
+    public Response mapException(final Exception exception) {
+        exception.printStackTrace();
+        return Response.serverError()
+                .entity(Map.of("message", "Erro interno ao processar requisição. Tente novamente."))
+                .build();
+    }
+
+}
