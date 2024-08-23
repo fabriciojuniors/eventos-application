d3380e7935d35a5e62c5017003d2b2db62316f1f
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-08-23 11:53:15 -0300

    Scripts para alimentação da base H2 (testes)

---
+++ src/test/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO instituicoes (id,nome,tipo) VALUES ('32758e52-a5b5-4a1e-9726-2a5dede7e448','Cresol','COOPERATIVA');
+INSERT INTO instituicoes (id,nome,tipo) VALUES ('6f3de683-89ab-45c5-9b3e-16f0b695cbc4','Para exclusão','COOPERATIVA');
+
+INSERT INTO public.eventos (id,ativo,dh_fim,dh_inicio,nome,i_instituicoes) VALUES
+('ba36587e-cca3-4fb7-980b-bc9db3c3b319',true,'2024-08-23 20:44:00','2025-08-22 20:44:00','Evento de teste','32758e52-a5b5-4a1e-9726-2a5dede7e448'),
+('d4dc4cbc-c0aa-491a-8ac8-84c4b47c92ea',false,'2024-08-23 20:44:00','2024-08-22 20:44:00','Evento de teste 2','32758e52-a5b5-4a1e-9726-2a5dede7e448'),
+('42150ea8-cbb1-4666-b138-dd28f1955d16',false,'2024-08-23 20:44:00','2024-08-22 20:44:00','Evento de teste 3','32758e52-a5b5-4a1e-9726-2a5dede7e448'),
+('a58ac2b8-54d5-475d-9cf9-7eb8f2b01ba3',false,'2024-08-23 20:44:00','2024-08-22 20:44:00','Evento de teste 4','32758e52-a5b5-4a1e-9726-2a5dede7e448'),
+('e1039dce-1226-4812-ba5c-250ddd8fb505',true,'2024-08-22 20:44:00','2024-08-22 20:44:00','Evento de teste 5','32758e52-a5b5-4a1e-9726-2a5dede7e448'),
+('7031f02b-9053-46c4-8535-786f3c8a6dd3',true,'2024-08-22 20:44:00','2024-08-22 20:44:00','Evento de teste 6','32758e52-a5b5-4a1e-9726-2a5dede7e448');
+
